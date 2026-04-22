2516fa8a3124a9ddd2da49b9af09fb42f5854c8c
oryzae24 <134453358+oryzae24@users.noreply.github.com>
2026-04-22 16:05:34 +0000

    implement excercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
